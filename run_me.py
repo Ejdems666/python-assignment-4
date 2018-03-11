1b7d3885733540a2bfa209486b79c539d3a06280
Adam Becvar <becvar.adam666@gmail.com>
2018-03-11 21:33:04 +0100

    Refactor run_me

--- run_me.py
+++ run_me.py
@@ -4,19 +4,25 @@ import question3
 from file_handler import download_csv_sheet
 
 
+def run():
+    csv_sheet_name = 'data.csv'
+    url = 'https://raw.githubusercontent.com/INFINITE-KH/Python-Dataset/master/complete.csv'
+    download_csv_sheet(csv_sheet_name, url)
+    with open(csv_sheet_name, encoding='utf-8') as f:
+        print_question_separator('1. Richest and poorest teams by cumulative player worth')
+        question1.run(f)
+
+        print_question_separator('2. Nationality with most players')
+        question2.run(f)
+
+        print_question_separator('3. Difference between worth and release clause of top 10 valuable players')
+        question3.run(f)
+
+
 def print_question_separator(question_number):
-    print('\nQuestion ' + str(question_number), end='\n' + 100 * '-' + '\n')
+    print('\nQuestion ' + str(question_number), end='\n' + 100 * '-' + '\n')˚
 
 
-csv_sheet_name = 'data.csv'
-url = 'https://raw.githubusercontent.com/INFINITE-KH/Python-Dataset/master/complete.csv'
-download_csv_sheet(csv_sheet_name, url)
-with open(csv_sheet_name, encoding='utf-8') as f:
-    print_question_separator('1. Richest and poorest teams by cumulative player worth')
-    question1.run(f)
-
-    print_question_separator('2. Nationality with most players')
-    question2.run(f)
-
-    print_question_separator('3. Difference between worth and release clause of top 10 valuable players')
-    question3.run(f)
+run()
+# It is really bugging me that you have to define function before you call it, so I wrapped everything in run()
+# Thanks to that I can order functions as they are called, so you can read code from top to bottom
